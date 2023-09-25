f4c39631e002213a8dc9618df594e55528712c81
arabirova <arabirova@inbox.ru>
2023-09-26 00:54:04 +0300

    test

--- travel.me/travel.me/Models/RouteModel.swift
+++ travel.me/travel.me/Models/RouteModel.swift
@@ -12,11 +12,13 @@ class RouteModel {
     var name: String
     var description: String
     var detailDescription: String
+    var coordinate: String
     
-    init(imageURL: String, name: String, description: String, detailDescription: String) {
+    init(imageURL: String, name: String, description: String, detailDescription: String, coordinate: String) {
         self.imageURL = imageURL
         self.name = name
         self.description = description
         self.detailDescription = detailDescription
+        self.coordinate = coordinate
     }
 }
